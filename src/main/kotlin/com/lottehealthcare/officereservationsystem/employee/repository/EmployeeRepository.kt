9a58d81196c60b1c05d251c54b9193cc4a1dd3f5
sdoaolo <09jieun01@naver.com>
2023-11-18 08:52:44 +0900

    Refactor: Repository function - Nullable - Repository에 ?를 사용하지 않아, NULL이 발생할 수 없어 서비스단에서 비즈니스 예외를 throw하지 않는것을 확인했습니다. - Repository에서 NULL을 허용함으로써 서비스단에서 동일하게 예외처리 되도록 수정했습니다.

--- src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/repository/EmployeeRepository.kt
@@ -4,5 +4,5 @@ import com.lottehealthcare.officereservationsystem.employee.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface EmployeeRepository: JpaRepository<Employee, Long> {
-    fun findByEmployeeNumber(employeeNumber: Short): Employee
+    fun findByEmployeeNumber(employeeNumber: Short): Employee?
 }
