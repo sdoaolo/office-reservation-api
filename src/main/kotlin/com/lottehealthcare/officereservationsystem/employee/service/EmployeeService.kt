3d814971215efff798cb987803f5214bc6c1f39c
sdoaolo <09jieun01@naver.com>
2023-11-18 19:20:54 +0900

    Feat: paging work status - size = 20명으로 설정하여 페이징 처리했습니다.(* page는 1부터 조회) - <오피스출근>한 경우에만 좌석번호를 함께 반환하고 있습니다.

--- src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/service/EmployeeService.kt
+++ src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/service/EmployeeService.kt
@@ -2,8 +2,9 @@ package com.lottehealthcare.officereservationsystem.employee.service
 
 import com.lottehealthcare.officereservationsystem.employee.dto.response.CurrentWorkStatusDto
 import com.lottehealthcare.officereservationsystem.employee.entity.Employee
+import org.springframework.data.domain.Page
 
 interface EmployeeService {
     fun registerNewEmployee(employee: Employee): Employee
-    fun getAllEmployeeWorkStatus():  List<CurrentWorkStatusDto>
+    fun getAllEmployeeWorkStatus(page: Int): Page<CurrentWorkStatusDto>
 }
