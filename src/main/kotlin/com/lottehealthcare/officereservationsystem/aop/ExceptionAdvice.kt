2a0330c7d70993a3ec28db088ba9f92d8d4b9081
sdoaolo <09jieun01@naver.com>
2023-11-19 08:32:01 +0900

    feat: custom response 400(BAD_REQUEST)

--- src/main/kotlin/com/lottehealthcare/officereservationsystem/aop/ExceptionAdvice.kt
+++ src/main/kotlin/com/lottehealthcare/officereservationsystem/aop/ExceptionAdvice.kt
@@ -1,15 +1,36 @@
 package com.lottehealthcare.officereservationsystem.aop
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import com.lottehealthcare.officereservationsystem.common.ApplicationResponseDto
 import com.lottehealthcare.officereservationsystem.common.ResponseStatus
 import com.lottehealthcare.officereservationsystem.error.exception.BusinessException
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.NoHandlerFoundException
 import javax.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
 class ExceptionAdvice {
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadable(
+        e: HttpMessageNotReadableException,
+        request: WebRequest
+    ): ApplicationResponseDto<Any?> {
+        val errorMessage = when (val cause = e.cause) {
+            is MismatchedInputException -> "${cause.path.joinToString() { it.fieldName }} is null"
+            else -> "유효하지 않은 요청입니다"
+        }
+        return ApplicationResponseDto(
+            status = ResponseStatus.BAD_REQUEST,
+            message = errorMessage,
+            code = ResponseStatus.BAD_REQUEST.code,
+            isSuccess = false,
+            data = null
+        )
+    }
 
     @ExceptionHandler(NoHandlerFoundException::class)
     fun handleUrlNotFoundException(
