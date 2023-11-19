2fd96a67ffddc85f26af52e322aeec835a24ca6e
sdoaolo <09jieun01@naver.com>
2023-11-20 08:53:57 +0900

    feat: documentation (swagger api)

--- src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/controller/EmployeeController.kt
+++ src/main/kotlin/com/lottehealthcare/officereservationsystem/employee/controller/EmployeeController.kt
@@ -6,15 +6,20 @@ import com.lottehealthcare.officereservationsystem.employee.dto.request.Register
 import com.lottehealthcare.officereservationsystem.employee.dto.response.CurrentWorkStatusDto
 import com.lottehealthcare.officereservationsystem.employee.dto.response.SimpleImformationEmployeeDto
 import com.lottehealthcare.officereservationsystem.employee.service.EmployeeService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+@Api(tags = ["Employee API"])
 @RestController
 @RequestMapping("/api/v1/employees")
 class EmployeeController(
     private val employeeService: EmployeeService
 ){
+
     @PostMapping
+    @ApiOperation(value = "새 직원 등록", notes = "새로운 직원을 시스템에 등록합니다.")
     fun registerNewEmployee(@RequestBody @Valid registerEmployee: RegisterNewEmployeeDto): ApplicationResponseDto<SimpleImformationEmployeeDto> {
 
         return ApplicationResponseDto(
@@ -25,8 +30,8 @@ class EmployeeController(
             employeeService.registerNewEmployee(registerEmployee.toEntity())
         )
     }
-    @GetMapping
-    @RequestMapping("/work-status")
+    @GetMapping ("/work-status")
+    @ApiOperation(value = "현재 직원 근무 상태 조회", notes = "직원들의 근무 상태를 조회합니다. 페이지 번호를 매개변수로 제공해야하며, 페이지는 1번부터 조회 가능합니다.")
     fun getAllEmployeeWorkStatus(@RequestParam("page") page: Int) : ApplicationResponseDto<List<CurrentWorkStatusDto>> {
 
         return ApplicationResponseDto(
